e649b6b3623a5ca202d51d837f184165e1af8b38
conaticus <samalbertdev@gmail.com>
2022-01-23 18:02:28 +0000

    add git option

--- src/cli.h
+++ src/cli.h
@@ -6,6 +6,7 @@
 typedef enum
 {
     NODE,
+    GIT,
 } CLIArgumentType;
 
 typedef struct
--- src/main.c
+++ src/main.c
@@ -3,23 +3,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 void arg_template_init()
 {
     cli_arg_template_add("n", "node", false, NODE);
+    cli_arg_template_add("g", "git", true, GIT);
 }
 
 void execute_args()
 {
-    if (cli_get_arg(NODE)) {
-        /*
-            Would you like authentication (y/N): 
-                Please choose an authentication type:
-                    1. JSON Web Token
+    CLIArgument* git_arg = cli_get_arg(GIT);
 
-                Enter type:
-        */
+    // Change later to support upstream
+    if (git_arg != NULL) {
+        system("git init");
+        system(strcat("git remote add origin", git_arg->value));
+    }
 
+    if (cli_get_arg(NODE) != NULL) {
        if (input_choice("Would you like authentication in your application", NO) == YES) {
            char* auth_options[] = { "JSON Web Token" };
            int auth_choice = input_multiple_choice("Please choose an authentication type", auth_options, sizeof(auth_options) / sizeof(char*));
